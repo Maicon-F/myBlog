f57856d4ff16734b47737a87e937ac4df8971ed6
Maicon <hrqn.fernandes@gmail.com>
2021-06-07 12:38:43 -0300

    Corrigida importacao errada da biblioteca NotNull e adicionado validacao de entradas

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
-	public Optional <Usuario> findByusuario(String usuario);
+	public Optional <Usuario> findByUsuario(String usuario);
 
 }
