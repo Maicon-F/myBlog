dc5ccd24b4882000d7a44dd2ccb87dd266fb9edf
Maicon <hrqn.fernandes@gmail.com>
2021-06-10 10:21:39 -0300

    adicionado método buscar por id no servele

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -4,14 +4,11 @@ import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
-import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuarios")
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins ="*",allowedHeaders ="*")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins ="*",allowedHeaders ="*")
 public class UsuarioController {
 
 	
@@ -37,8 +34,6 @@ public class UsuarioController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.cadastrarUsuario(usuario));
 	}
-	
-	
 	
 	
 }
