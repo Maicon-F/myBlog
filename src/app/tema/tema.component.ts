afcd0a4abb2839df21d39017b0420ad34358e16b
Maicon <hrqn.fernandes@gmail.com>
2021-06-23 01:08:06 -0300

    mensagem

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -47,7 +47,7 @@ export class TemaComponent implements OnInit {
  cadastrar(){
   this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
     this.tema = resp
-    alert('Tema cadastrado com sucesso!')
+    this.alertas.showAlertSuccess('Tema cadastrado com sucesso!')
     this.findAllTemas()
     this.tema = new Tema() 
   })
